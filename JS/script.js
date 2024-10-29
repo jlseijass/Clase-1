136b0a7dbd53ce9c564eaa8d9209e6ad48381f37
Jose Seijas <jose.seijas@parkandbattery.com>
2024-10-29 10:53:43 -0600

    Agregando el ejercicio correcto

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,13 @@
-const nombre = parseInt(prompt("Ingresa tu nombre: "));
-const apellido = parseInt(prompt("Ingresa tu apellido: "));
-const edad = parseInt(prompt("Ingresa tu edad: "));
+const usuarios = [
+{nombre: "Ana", edad:20,aceptoTerminos:false},
+{nombre: "Luis", edad:17,aceptoTerminos:true},
+{nombre: "Carlos", edad:18,aceptoTerminos:true},
+{nombre: "María", edad:15,aceptoTerminos:true},
+{nombre: "Angela", edad:21,aceptoTerminos:true}
+]
 
-const resultado = nombre+apellido+edad
-
-const mensaje = "Hola " + nombre + " " apellido + " .Bienvenido a nuestra base de datos. Tu edad es de " + edad + " años"
-
-alert(mensaje)
+for(let i = 0; i <usuarios.length; i++) {
+   if(usuarios[i].edad >=18 && usuarios[i].aceptoTerminos) {
+    alert(usuarios[i].nombre);
+   }
+}

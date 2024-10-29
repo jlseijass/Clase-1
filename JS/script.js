aa2af1a1cc2e868a268c8acf81c88ca04b5f0495
Jose Seijas <jose.seijas@parkandbattery.com>
2024-10-29 11:03:53 -0600

    Cambiando el mensaje

--- JS/script.js
+++ JS/script.js
@@ -3,8 +3,8 @@ const lastName = prompt("Ingresa tu apellido:");
 const age = parseInt(prompt("Ingresa tu edad:"));
 const years = parseInt(prompt("Cuantos años más pasarán en 4 años?:"));
 
-// Calculate the future age
+// Calculando la edad futura
 const futureAge = age + years;
 
-// Display the final message
+// Muestra el mensaje
 alert(`Hola, ${firstName} ${lastName}. Bienvenido a nuestra base de datos. Tu edad es ${age} años. Dentro de ${years} años, vas a tener ${futureAge}.`);
